c16cccb143465768ccf043b29efe89bc29ad1760
NataliaVozhdaeva <LomzNat@yandex.ru>
2022-11-10 22:35:25 +0200

    create navigation Recipes/shoppingList

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,17 +11,17 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'testRecipeName',
       'testRecipe Description',
-      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.pxhere.com%2Fphotos%2Fc4%2Fed%2Fsushi_sashimi_food_oriental_combined_barca_japanese-625376.jpg!s&f=1&nofb=1&ipt=0500182dd56a19d091ae75a0b3c5b906efda7e30645f3714f13431a9e2975276&ipo=images'
     ),
     new Recipe(
       'Other Recipe name',
       'Other Recipe',
-      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.pxhere.com%2Fphotos%2Fc4%2Fed%2Fsushi_sashimi_food_oriental_combined_barca_japanese-625376.jpg!s&f=1&nofb=1&ipt=0500182dd56a19d091ae75a0b3c5b906efda7e30645f3714f13431a9e2975276&ipo=images'
     ),
     new Recipe(
       'Ones more RecipeName',
       'And ones more Description',
-      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.pxhere.com%2Fphotos%2Fc4%2Fed%2Fsushi_sashimi_food_oriental_combined_barca_japanese-625376.jpg!s&f=1&nofb=1&ipt=0500182dd56a19d091ae75a0b3c5b906efda7e30645f3714f13431a9e2975276&ipo=images'
     ),
   ];
 
